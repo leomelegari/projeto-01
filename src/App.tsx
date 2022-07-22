b6ce5a3e21f7f8082f17cc9e0e7e153284db5c28
Leonardo Melegari Deffende <leo_melegari@outlook.com>
2022-07-22 12:27:06 -0300

    feat: created profile aside and its stylizations

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 import "./global.css";
+import styles from "./App.module.css";
 
 import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
 
 const App = () => {
   return (
     <div>
       <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <div>Profile</div>
+          <div>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique
+            blanditiis accusantium assumenda enim cumque. Sit perspiciatis
+            culpa, consequuntur explicabo perferendis dolorum saepe nemo
+            temporibus provident expedita, quis incidunt animi enim.
+          </div>
+        </main>
+      </div>
     </div>
   );
 };
