69ae1a6d78521d107baf4fc3163bbfe0e0791767
Leonardo Melegari Deffende <leo_melegari@outlook.com>
2022-07-23 13:16:02 -0300

    feat: half of the post section created

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import styles from "./App.module.css";
 
 import { Header } from "./components/Header";
 import { Sidebar } from "./components/Sidebar";
+import { Post } from "./components/Post";
 
 const App = () => {
   return (
@@ -12,13 +13,8 @@ const App = () => {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <div>Profile</div>
-          <div>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique
-            blanditiis accusantium assumenda enim cumque. Sit perspiciatis
-            culpa, consequuntur explicabo perferendis dolorum saepe nemo
-            temporibus provident expedita, quis incidunt animi enim.
-          </div>
+          <Post />
+          <Post />
         </main>
       </div>
     </div>
