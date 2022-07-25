0ea9d34a8d60bf8e3b3097b43407f6a2940f6840
Leonardo Melegari Deffende <leo_melegari@outlook.com>
2022-07-25 13:39:50 -0300

    feat: post now is conditionaly rendering its values

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,49 @@ import { Header } from "./components/Header";
 import { Sidebar } from "./components/Sidebar";
 import { Post } from "./components/Post";
 
+// author: {avatar_url: "", name: "", role: ""}
+// publishedAt: date
+// content: string
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/leomelegari.png",
+      name: "Leo Melegari",
+      role: "React developer",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "👉  jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2022-07-25 08:57:35"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/maykbrito.png",
+      name: "Mayk Brito",
+      role: "React developer",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "👉  jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2022-07-08 17:20:15"),
+  },
+];
+
 const App = () => {
   return (
     <div>
@@ -13,8 +56,15 @@ const App = () => {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
-          <Post />
+          {posts.map((post) => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
         </main>
       </div>
     </div>
